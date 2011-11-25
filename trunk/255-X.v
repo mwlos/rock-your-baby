6ec59b1773632ff09c38475b5e94190672f080bb
stevenvdschoot@gmail.com <stevenvdschoot@gmail.com@d34399de-3a98-3ead-f736-68d437feb995>
2011-11-25 13:37:14 +0000

    Mijn Verilog sub mudoles

---
+++ trunk/255-X.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:24:24 11/22/2011 
+// Design Name: 
+// Module Name:    255-X-8 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module 255_X8(
+    input [7:0] in,
+    input [7:0] out
+    );
+
+assign out = (255-in);
+
+endmodule
---
+++ trunk/multShift.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:26:09 11/22/2011 
+// Design Name: 
+// Module Name:    multShift-8 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module multShift_8(
+    input [7:0] a,
+    input [7:0] b,
+    output [7:0] q
+    );
+
+assign q = ( (a*b) << 8 );
+
+endmodule
