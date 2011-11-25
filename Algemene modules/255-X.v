4db69a2e9d71a6c03978b446277a45ffb8dae205
stevenvdschoot <stevenvdschoot@gmail.com>
2011-11-25 13:47:58 +0000

    Files geupdate

--- Algemene modules/255-X.v
+++ Algemene modules/255-X.v
@@ -1,23 +1,7 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    16:24:24 11/22/2011 
-// Design Name: 
-// Module Name:    255-X-8 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+/**
+ * Copyright: Steven van der Schoot
+ */
 module 255_X8(
     input [7:0] in,
     input [7:0] out
--- Algemene modules/GT.v
+++ Algemene modules/GT.v
@@ -1,23 +1,7 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    16:13:37 11/22/2011 
-// Design Name: 
-// Module Name:    GT-8 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+/**
+ * Copyright: Steven van der Schoot
+ */
 module GT8(
     input [7:0] a,
     input [7:0] b,
