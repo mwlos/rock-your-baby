8f5ec27e49bb976bd36ae641c84affe8faf91c6c
Bart.tiemersma <Bart.tiemersma@gmail.com>
2011-11-29 15:03:39 +0000

    Default toegevoegd

--- Output/WaardeNaarPWM.v
+++ Output/WaardeNaarPWM.v
@@ -7,11 +7,12 @@ module toestandKeuze(
 	
 	always @ (Invoer) begin
 		case (Invoer) 
-			0: PWM = 1200;	// 5%
-			1: PWM = 4800;	// 20%
-			2: PWM = 9600;	// 40%
-			3: PWM = 14400;	// 60%
-			4: PWM = 19200;	// 80%
+			0: PWM = 1200;			// 5%
+			1: PWM = 4800;			// 20%
+			2: PWM = 9600;			// 40%
+			3: PWM = 14400;			// 60%
+			4: PWM = 19200;			// 80%
+			default: PWM = 19200; 	// default 80%
 		endcase
 	end
 	
