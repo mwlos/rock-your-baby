3645dae5bd6622c25688aad5777dcbfb30da81b9
Bart.tiemersma <Bart.tiemersma@gmail.com>
2011-11-29 16:32:32 +0000

    Foute declaraties opgelost.
    ; fout was toch goed.

--- Output/Output.v
+++ Output/Output.v
@@ -1,17 +1,10 @@
 module Output(
 	input CLK,
 	input Reset,
-    input Freq,
-	input Amp,
-    output fPWM,
-	output aPWM)
-	 
-	input CLK;
-	input Reset;
-	input [2:0] Freq;
-	input [2:0] Amp;
-	output fPWM;
-	output aPWM;
+	input [2:0] Freq,
+	input [2:0] Amp,
+	output fPWM,
+	output aPWM);
 	
 	wire [15:0] f;
 	wire [15:0] a;
--- Output/WaardeNaarPWM.v
+++ Output/WaardeNaarPWM.v
@@ -1,19 +1,20 @@
 module WaardeNaarPWM(
-    input Invoer,
-    output PWM)
+    input [2:0] Invoer,
+    output [15:0] PWM);
 	 
-	input [2:0] Invoer;
-	output reg [15:0] PWM;
+	reg [15:0] Uitgang;
 	
 	always @ (Invoer) begin
 		case (Invoer) 
-			0: PWM = 1200;			// 5%
-			1: PWM = 4800;			// 20%
-			2: PWM = 9600;			// 40%
-			3: PWM = 14400;			// 60%
-			4: PWM = 19200;			// 80%
-			default: PWM = 19200; 	// default 80%
+			0: Uitgang = 1200;			// 5%
+			1: Uitgang = 4800;			// 20%
+			2: Uitgang = 9600;			// 40%
+			3: Uitgang = 14400;			// 60%
+			4: Uitgang = 19200;			// 80%
+			default: Uitgang = 19200; 	// default 80%
 		endcase
 	end
+	
+	assign PWM = Uitgang;
 	
 endmodule
