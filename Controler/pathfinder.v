3e1d9214207a4db2a112872d46fe4015b4891291
stevenvdschoot <stevenvdschoot@gmail.com>
2011-12-02 14:23:57 +0000

    pathfinder module nu ook toegevoegd :D

---
+++ Controler/pathfinder.v
@@ -0,0 +1,25 @@
+module pathfinder(
+	input clk,
+	input reset,
+	input stress,
+	input Flow,
+	output Fmin
+	output Fplus
+	output Amin)
+	
+	wire data;
+	reg wrong;
+	
+	assign data = ( Flow | wrong ) & ( (~Flow) | stress);
+	assign Fmin = (~reset) | ( lck | ( (~Flow) | stress ) );
+	assign Fplus = (~reset) | data;
+	assign Amin = (~reset) | ( (~( (~Flow) | stress )) | ( Flow | wrong ) );
+	
+	always @ (posedge clk or posedge reset) begin
+		if (reset)
+			wrong = 0;
+		else
+			wrong = data;
+	end
+	
+endmodule
