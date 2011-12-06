1cf1bec8915f264c0f4e305de3e3f1c050ba6883
Bart.tiemersma <Bart.tiemersma@gmail.com>
2011-12-06 12:54:26 +0000

    Vertraagde clock geïmplementeerd

--- totaal.v
+++ totaal.v
@@ -11,15 +11,15 @@ module totaal (
 	wire [7:0] huilVol;
 	wire [2:0] amp;
 	wire [2:0] freq;
+	
+	clkDelay	  delay(clk		, reset, slowClk								 );
 
-	huilVolume	  huil (clk, reset, DSPingang,		DSPctrl,  huilVol       ); 
-	hartRitme	  hart (clk, reset, hartslagIngang,	hartslag                );
+	huilVolume	  huil (slowClk	, reset, DSPingang,		DSPctrl,  huilVol        ); 
+	hartRitme	  hart (clk		, reset, hartslagIngang,	hartslag             );
 	
-	FPGAControler crtl (clk, reset, huilVol,        hartslag, amp,    freq  );
-	Output        out  (clk, reset, freq,           amp,      PSfreq, PSamp );
+	FPGAControler crtl (slowClk	, reset, huilVol,        hartslag, amp,    freq  );
+	Output        out  (clk		, reset, freq,           amp,      PSfreq, PSamp );
 	
-	// Er moet een klokvertraging ingebouwd worden.
-	// De modules Output en hartRitme moeten wel gewoon aan de systeemklok aangesloten worden.
 	// Verder moet er een Reset ingebouwd worden om er voor te zorgen dat de regeling opnieuw begint zodra we het pad kwijt raken.
 	
 endmodule
