2d6f995fc59220e812dee17e2f345f277359d9e8
Bart.tiemersma <Bart.tiemersma@gmail.com>
2011-12-09 15:15:40 +0000

    Alle ingangen aangesloten via een FF.

--- Invoer/Huil volume.v
+++ Invoer/Huil volume.v
@@ -4,20 +4,36 @@ module huilVolume(
 	input reset,
 	input [7:0] DSPinvoer,
 	input DSPready,
-	output reg [7:0] huilVolume);
+	output [7:0] huilVolume);
 	
 	reg [13:0] Memory;
+	reg ready;
+	reg [7:0] DSP;
 	wire resetmore;
 	
 	assign resetmore = ( reset | slowClk );
-
-	always @ (posedge DSPready or posedge resetmore) begin
-		if (reset)
-			Memory = 0;
+	
+	always @ (posedge clk or posedge resetmore) begin
+		if (resetmore)
+			ready = 0;
 		else
-			Memory = Memory + DSPinvoer;
+			ready = DSPready;
 	end
 	
-	assign huilVolume = [13:6] Memory;
+	always @ (posedge clk or posedge resetmore) begin
+		if (resetmore)
+			DSP = 0;
+		else
+			DSP = DSPinvoer;
+	end
+
+	always @ (posedge ready or posedge resetmore) begin
+		if (resetmore)
+			Memory = 0;
+		else
+			Memory = Memory + DSP;
+	end
+	
+	assign huilVolume = Memory[13:6];
 	
 endmodule
