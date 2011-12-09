546a53bf9b20e1def165f563692e51d32d86fccd
stevenvdschoot <stevenvdschoot@gmail.com>
2011-12-09 12:04:03 +0000

    holdReset.v toegevoegd

--- totaal.v
+++ totaal.v
@@ -1,6 +1,7 @@
 module totaal (
 	input clk,
 	input reset,
+	input start,
 	input [7:0] DSPingang,
 	input hartslagIngang,
 	input [7:0] ClockSnelheid,
@@ -15,15 +16,18 @@ module totaal (
 	wire slowClk;
 	wire error;
 	wire genReset;
+	wire intReset;
 	
 	assign genReset = (reset | error);
 	
-	clkDelay	  delay(clk		, genReset, ClockSnelheid,		slowClk				 					);
-
-	huilVolume	  huil (slowClk	, genReset, DSPingang,			DSPctrl,  		huilVol    				); 
-	hartRitme	  hart (clk		, slowClk,	genReset, 			hartslagIngang,	hartslag             	);
+	holdReset     starter(clk   , gerReset, start,              intReset                                );
 	
-	FPGAControler crtl (slowClk	, genReset, huilVol,        	hartslag, 		amp,    freq  , error	);
-	Output        out  (clk		, genReset, freq,           	amp,      		PSfreq, PSamp 			);
+	clkDelay	  delay(clk		, intReset, ClockSnelheid,		slowClk				 					);
+
+	huilVolume	  huil (slowClk	, intReset, DSPingang,			DSPctrl,  		huilVol    				); 
+	hartRitme	  hart (clk		, slowClk,	intReset, 			hartslagIngang,	hartslag             	);
+	
+	FPGAControler crtl (slowClk	, intReset, huilVol,        	hartslag, 		amp,    freq  , error	);
+	Output        out  (clk		, intReset, freq,           	amp,      		PSfreq, PSamp 			);
 	
 endmodule
