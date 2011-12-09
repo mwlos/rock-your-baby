74efda0dbaabbbd6398a64c68688d3156f6fbf2b
mb.mishabekker <mb.mishabekker@gmail.com>
2011-12-09 14:31:25 +0000

    nieuwe deltaStress

--- Controler/deltaStress.v
+++ Controler/deltaStress.v
@@ -1,73 +1,29 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    16:16:42 12/02/2011 
-// Design Name: 
-// Module Name:    deltaStress 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 module deltaStress(
 		input clk,
-		input r,
-		input[7:0] huilVolume,
-		input[7:0] hartslag,
-		output stressLaag
-    );
-	 
-	 reg [2:0] huilOud;
-	 reg hartOut;
-	 reg huilOut;
-
-	 wire [2:0] huil;
-	 wire [2:0] hart;
-	 reg [2:0] hartOud;
-	 wire hartLaag;
-	 
-	 
-	 assign huil = huilVolume[7:5];	// Geeft warning misschien een shift gebruiken. Misschien is deze warning ook te negeren.
-	 assign hart = hartslag[3:1];   // Zelfde
+		input reset,
+		input status[2:0],
+		output gedaald,
+		output gelijk
+		);
 		
-	/*
-	always @ (posedge clk or posedge r) begin
-		if (r) huilOud = 0;
-		else huilOud = huil;
-	end
+	wire[2:0] data;	
+	wire[2:0] dataDly;
+	wire const;
+	wire constDly;
 	
-	always @ (posedge clk or posedge r) begin
-		if (r) hartOud = 0;
-		else hartOud = hart;
-	end
-	*/
+	assign data = status;
 	
-	assign huilLaag = (huil < huilOut);
-	assign hartLaag = (hart < hartOut);
+	delay_3 delay1 (clk, reset, data, dataDly);
 	
-	always @ (posedge clk or posedge r) begin
-		if (r)
-			huilOut = 0;
-		else
-			huilOut = huilLaag;
-	end
+	assign gedaald = (data < dataDly);	
+	assign const = (data==dataDly);
 	
-	always @ (posedge clk or posedge r) begin
-		if (r)
-			hartOut = 0;
-		else
-			hartOut = hartLaag;
-	end
+	delay_1 delay2 (clk, reset, const, constDly);
 	
-	assign stressLaag = clk & (huilLaag | hartLaag);
-
+	assign gelijk = (const&constDly);
+	
 endmodule
+	
+	
+	
+	
