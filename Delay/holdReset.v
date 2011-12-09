2990503bc896456485f1893a7777d626278bc033
stevenvdschoot <stevenvdschoot@gmail.com>
2011-12-09 12:16:31 +0000

    holdReset.v af

--- Delay/holdReset.v
+++ Delay/holdReset.v
@@ -2,8 +2,13 @@ module holdReset(
 	input clk,
 	input resetSignal,
 	input stopHold,
-	output reset);
+	output reg reset);
 	
-	
+	always @ (posedge clk or posedge stopHold) begin
+		if (stopHold)
+			reset = 0;
+		else if (resetSignal)
+			reset = 1;
+	end
 	
 endmodule
