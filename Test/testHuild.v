7325c6da88b55725277a9e95cf0a6d94e257d44f
Bart.tiemersma <Bart.tiemersma@gmail.com>
2011-12-11 18:50:32 +0000

    Test module geschreven voor het huilvolume

--- Test/testHuild.v
+++ Test/testHuild.v
@@ -2,13 +2,14 @@ module testHuil (
 	input clk,
 	input reset,
 	input [7:0] DSPingang,
-	input [7:0] ClockSnelheid,
+	input DSPready
 	output [7:0] huilVol
 	);
 	
-	wire slowClk;
+	wire slow4;
+	wire slow12;
 	
-	clkDelay	  delay(clk		, reset, ClockSnelheid,		slowClk				 	);
-	huilVolume	  huil (slowClk	, reset, DSPingang,			DSPctrl,  huilVol    	);
+	clkDelay	  delay(clk		, reset		, slow4, slow12				 			);
+	huilVolume	  huil (clk		, slowClk	, reset, DSPingang,	DSPready,  huilVol  );
 	
 endmodule
