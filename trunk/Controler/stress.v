33ed01680670141220b34722201786a37281c0f2
stevenvdschoot@gmail.com <stevenvdschoot@gmail.com@d34399de-3a98-3ead-f736-68d437feb995>
2011-12-12 12:56:50 +0000

    Aansluitingen geupdate

---
+++ trunk/Controler/stress.v
@@ -0,0 +1,28 @@
+module stress(
+		input clk,
+		input reset,
+		input clk4,
+		input clk12,
+		input [8:0] huil,
+		input hart,
+		output gedaald,
+		output gelijk
+		);
+	
+	wire [3:0] hartData;
+	wire [7:0] huilData;
+	wire hartLaag;
+	wire huilLaag;
+	wire hartGelijk;
+	wire huilGelijk;
+	
+	hartRitme  hartRit (clk,clk4,reset,hart,hartData);
+	huilVolume huilVol (clk,clk4,reset,huil,DSPctrl,huilData);
+	
+	deltaStressHart hart (reset,clk12,hartData,hartLaag,hartGelijk);
+	deltaStressHuil huil (reset,clk4,clk12,huilData,huilLaag[7:4],huilGelijk);
+	
+	assign gedaald = (hartLaag|huilLaag);
+	assign gelijk  = (hartGelijk|huilGelijk);
+	
+endmodule
