6e26cc46d356bbb2340bb69f9cccd27b5266f7fd
stevenvdschoot@gmail.com <stevenvdschoot@gmail.com@d34399de-3a98-3ead-f736-68d437feb995>
2011-12-13 13:54:12 +0000

    Commit 2

---
+++ trunk/NEW/Controler/FPGAControler.v
@@ -0,0 +1,22 @@
+module FPGAControler(
+	input clk,
+	input reset,
+	input stressGezakt,
+	output [2:0] A,
+	output [2:0] F,
+	output err
+	);
+	
+	wire Alaag;
+	wire Flaag;
+	wire Fhoog;
+	wire res;
+	wire err1;
+	wire err2;
+	
+	assign err = (err1 | err2);
+	
+	pathFinder  pathfind ( clk, reset | res , stressGezakt, Alaag    , Fhoog       , Flaag, err1     );
+	FAG         math     ( clk, reset       , Alaag       , Fhoog    , Flaag       , A    , F  , res, err2);
+	
+endmodule
