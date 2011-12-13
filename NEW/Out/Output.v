748ebbf44d37fec26c0564e187ad48c7ab02bf80
stevenvdschoot <stevenvdschoot@gmail.com>
2011-12-13 13:55:35 +0000

    COmmit last

---
+++ NEW/Out/Output.v
@@ -0,0 +1,18 @@
+module Output(
+	input CLK,
+	input Reset,
+	input [2:0] Freq,
+	input [2:0] Amp,
+	output fPWM,
+	output aPWM);
+	
+	wire [15:0] f;
+	wire [15:0] a;
+	
+	WaardeNaarPWM 	 freqConv	(Freq, 		f);
+	signaalGenerator freqPWM	(CLK, 	Reset,	f,	 fPWM);
+	
+	WaardeNaarPWM 	 ampConv 	(Amp , 		a);
+	signaalGenerator ampPWM		(CLK,	Reset,	a,	 aPWM);
+	
+endmodule
---
+++ NEW/Totaal.v
@@ -0,0 +1,40 @@
+module totaal (
+	input clk,
+	input reset,
+	input [7:0] DSPingang,
+	input hartslagIngang,
+	input DSPready,
+	output PSfreq,
+	output PSamp);
+	
+	wire [2:0] amp;
+	wire [2:0] freq;
+	
+	wire slow4;
+	wire slow12;
+	wire error;
+	wire intReset;
+	wire extReset;
+	wire stressLaag;
+	wire stressContinu;
+	
+	
+	// Reset en Delay modules
+	
+	bufferReset	  buffer (clk		, reset,	error, intReset,	extReset);
+	holdReset     starter(clk   	, extReset, stressContinu,      intReset);
+	clkDelay	  delay	 (clk		, extReset, slow4,				slow12  );
+	
+	// Input modules
+
+	stress	  Ingang (clk, extReset, slow4, slow12, DSPingang, DSPready, hartslagIngang, stressLaag, stressContinu); 
+	
+	// Controler
+	
+	FPGAControler crtl 	 (slow4, intReset, stressLaag, amp, freq, error);
+	
+	// Output module
+	
+	Output        out 	 (clk, extReset, freq, amp, PSfreq, PSamp);
+	
+endmodule
