3e25b383b6994eef7dd54420dd4d0470a2c28059
stevenvdschoot@gmail.com <stevenvdschoot@gmail.com@d34399de-3a98-3ead-f736-68d437feb995>
2011-12-15 11:39:16 +0000

    Opzet voor vandaag

--- trunk/NEW/In/stress.v
+++ trunk/NEW/In/stress.v
@@ -7,23 +7,31 @@ module stress(
 		input DSPctrl,
 		input hart,
 		output gedaald,
-		output gelijk
+		output gelijk,
+		output error
 		);
 	
-	wire [3:0] hartData;
-	wire [7:0] huilData;
+	wire [7:0] hartData;
+	//wire [7:0] huilData;
 	wire hartLaag;
-	wire huilLaag;
+	//wire huilLaag;
 	wire hartGelijk;
-	wire huilGelijk;
+	//wire huilGelijk;
+	wire hartError
+	//wire huilError;
 	
 	hartRitme  hartRit (clk,clk4,reset,hart,hartData);
-	huilVolume huilVol (clk,clk4,reset,huil,DSPctrl,huilData);
+	//huilVolume huilVol (clk,clk4,reset,huil,DSPctrl,huilData);
 	
-	deltaStressHart hart (reset,clk12,hartData,hartLaag,hartGelijk);
-	deltaStressHuil huil (reset,clk4,clk12,huilData,huilLaag[7:4],huilGelijk);
+	deltaStressHart hart (reset,clk4,hartData,hartLaag,hartGelijk,hartError);
+	//deltaStressHuil huil (reset,clk4,clk12,huilData,huilLaag[7:4],huilGelijk);
 	
-	assign gedaald = (hartLaag|huilLaag);
-	assign gelijk  = (hartGelijk|huilGelijk);
+	//assign gedaald = (hartLaag|huilLaag);
+	//assign gelijk  = (hartGelijk|huilGelijk);
+	//assign error   = (hartError|huilError);
+	
+	assign gedaald = hartLaag;
+	assign gelijk  = hartGelijk;
+	assign error   = hartError;
 	
 endmodule
