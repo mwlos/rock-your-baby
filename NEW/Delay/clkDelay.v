6717c7932cee7281db511a184a7ae95e58fa1997
Bart.tiemersma <Bart.tiemersma@gmail.com>
2011-12-16 15:47:28 +0000

    2sec clock

--- NEW/Delay/clkDelay.v
+++ NEW/Delay/clkDelay.v
@@ -1,35 +1,22 @@
 module clkDelay (
 	input CLK,
 	input Reset,
-	output Slow4,
-	output Slow12);
+	output Slow);
 	
 	reg [27:0] C;
-	reg [1:0]  D;
+		
+	wire resetmore;
+		
+	assign resetmore = Reset | ( C >= 48000000 );
 	
-	wire reset4;
-	wire reset12;
-	
-	assign reset4 = reset12 | ( C >= 96000000 );
-	
-	always @ (posedge CLK or posedge reset4) begin
-		if(reset4)
+	always @ (posedge CLK or posedge resetmore) begin
+		if(resetmore)
 			C = 0;
 		else
 			C = C + 1;
 	end
 	
-	assign Slow4 = (C == 0);
+	assign Slow = (C == 0);
 	
-	assign reset12 = Reset | ( D >= 3 );
-
-	always @ (posedge Slow4 or posedge reset12) begin
-		if(reset12)
-			D = 0;
-		else
-			D = D + 1;
-	end
-	
-	assign Slow12 = ( (D == 0) & (C == 0) );
-	
+		
 endmodule
