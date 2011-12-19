0cfa2bc64ca5a4096dbe0e8349bb167c981dd0b5
stevenvdschoot@gmail.com <stevenvdschoot@gmail.com@d34399de-3a98-3ead-f736-68d437feb995>
2011-12-19 15:03:27 +0000

    Dit zou het continue signaal gelijk in een puls signaal gelijkPuls om moeten zetten.

--- trunk/NEW/In/deltaStressHart.v
+++ trunk/NEW/In/deltaStressHart.v
@@ -2,7 +2,7 @@ module deltaStressHart(
 		input slow,
 		input reset,
 		input [5:0] hart,
-		output gelijk,
+		output gelijkPuls,
 		output gedaald,
 		output error
 		);
@@ -10,23 +10,43 @@ module deltaStressHart(
 	wire [5:0] delayOne;
 	wire [5:0] delayTwo;
 	wire [5:0] delayThree;
+	wire gelijk;
+	wire resetSlower;
 	
 	reg  [5:0] memory;
+	reg gelijkHold;
+	reg slower;
 	
 	delay_6 delayModuleOne 		(slow, reset, hart, 	delayOne	);
 	delay_6 delayModuleTwo 		(slow, reset, delayOne, delayTwo	);
 	delay_6 delayModuleThree 	(slow, reset, delayTwo, delayThree	);
 	
+	assign resetSlower = reset | (slower == 7);
 	assign gelijk = ( (hart == delayOne) & (delayOne == delayTwo) & (delayTwo == delayThree) );
+	assign gelijkPuls = gelijk & gelijkHold;
 	
-	always @ (posedge gelijk or posedge reset) begin
+	always @ (posedge slow or posedge resetSlower) begin
+		if (resetSlower)
+			slower = 0;
+		else
+			slower = slower + 1;
+	end
+	
+	always @ (negedge clk or posedge resetSlower) begin
+			if (resetSlower)
+				gelijkHold = 0;
+			else
+				gelijkHold = ~gelijk;
+	end
+	
+	always @ (posedge gelijkPuls or posedge reset) begin
 		if (reset)
 			memory = 63;
 		else
 			memory = hart;
 	end
 	
-	assign gedaald  = ( hart > memory );
+	assign gedaald = ( hart > memory );
 	assign error	= ( hart < memory );
 	
 endmodule
